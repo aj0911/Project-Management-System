c98e67b49f851d0fb13d699993bbc8909c784918
abhinav jha <jhaabhinav16@gmail.com>
2024-02-10 13:59:53 +0530

    done html for home screen

---
+++ frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import './Home.css'
+import { useNavigate } from 'react-router-dom'
+import { features, footerLinks } from '../../Helper/Data';
+import { AiOutlineArrowRight } from 'react-icons/ai'
+import { FaAngleRight } from 'react-icons/fa'
+
+const Home = () => {
+    // states
+    const navigate = useNavigate();
+
+
+  return (
+    <div id="Home">
+        <header>
+            <div className="logo">
+                <img src={require('../../Assets/Images/logo.png')} alt="" />
+                <h1>DTU IRD</h1>
+            </div>
+            <nav>
+                <a href="#hero">Home</a>
+                <a href="#features">Features</a>
+                <a href="#Start">Start Project</a>
+            </nav>
+            <button onClick={()=>navigate('/Login')}>Login In</button>
+        </header>
+        <section id="hero">
+            <div className="top">
+                <h3>Discover of Project</h3>
+                <h2>Efficiency with <span>DTU-IRD</span></h2>
+                <p>Streamline project management with DTU IRD. From planning to execution, empower your team with efficient tools and collaborative workflows.</p>
+                <div className="btns">
+                    <button>Join as a Faculty</button>
+                    <button>Projects Preview</button>
+                </div>
+            </div>
+            <div className="bottom">
+                <img src={require('../../Assets/Images/hero-1.png')} alt="" />
+            </div>
+        </section>
+        <section id="features">
+            <h2>Features</h2>
+            <div className="data">
+                {
+                    features.map((feature,key)=>(
+                        <div key={key} className="feature">
+                            <h3>{feature.name}</h3>
+                            <p>{feature.text}</p>
+                            <img src={feature.img} alt="" />
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
+        <div id="Start">
+            <h3>Ready to Start your projects? Get Started Today!</h3>
+            <button>
+                <h3>Get Started</h3>
+                <AiOutlineArrowRight/>
+            </button>
+        </div>
+        <footer>
+            <div className="content">
+                <div className="left">
+                    <div className="logo">
+                        <img src={require('../../Assets/Images/logo.png')} alt="" />
+                        <h2>DTU IRD</h2>
+                    </div>
+                    <div className="newsletter">
+                        <h3>Get Update our Newsletter</h3>
+                        <div className="box">
+                            <input type="email" placeholder='Your email adress' />
+                            <button><FaAngleRight/></button>
+                        </div>
+                    </div>
+                </div>
+                <div className="right">
+                    {
+                        footerLinks.map(({name,links},i)=>(
+                            <div className="content">
+                                <h3 key={i}>{name}</h3>
+                                <div className="links">
+                                    {
+                                        links.map(({name,link},j)=>(
+                                            <a href={link} key={j}>{name}</a>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+            <p>&copy;DTU-IRD {(new Date(Date.now())).getFullYear()}, All Rights Reserved.</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/Helper/Data.js
@@ -0,0 +1,76 @@
+export const features = [
+    {
+        name:'Task Management',
+        text:'Organize tasks, set deadlines, and assign responsibilities to streamline project progress.',
+        img:require('../Assets/Images/hero-3.png')
+    },
+    {
+        name:'User-Friendly Interface',
+        text:'Offer an intuitive and user-friendly interface for seamless adoption and efficient utilization by team members at all levels.',
+        img:require('../Assets/Images/hero-app-2.png')
+    },
+    {
+        name:'Progress Tracking',
+        text:'Monitor project milestones, track progress in real-time, and identify potential bottlenecks for timely resolution.',
+        img:require('../Assets/Images/hero-2.png')
+    },
+    {
+        name:'Resource Allocation',
+        text:'Efficiently allocate resources, including personnel, time, and materials, for optimal project execution.',
+        img:require('../Assets/Images/hero-app-1.png')
+    },
+]
+
+export const footerLinks = [
+    {
+        name: 'Resources',
+        links: [
+            {
+                name: 'User Guides',
+                link: '#user-guides'
+            },
+            {
+                name: 'FAQs',
+                link: '#faqs'
+            },
+            {
+                name: 'Video Tutorials',
+                link: '#video-tutorials'
+            }
+        ]
+    },
+    {
+        name: 'Support',
+        links: [
+            {
+                name: 'Contact Us',
+                link: '#contact'
+            },
+            {
+                name: 'Help Center',
+                link: '#help-center'
+            },
+            {
+                name: 'Feedback',
+                link: '#feedback'
+            }
+        ]
+    },
+    {
+        name: 'Company',
+        links: [
+            {
+                name: 'About Us',
+                link: '#about'
+            },
+            {
+                name: 'Careers',
+                link: '#careers'
+            },
+            {
+                name: 'Press',
+                link: '#press'
+            }
+        ]
+    }
+];
